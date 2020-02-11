9ae8495673b42ca9158a8c2712af3c9a734021ec
Valdis <55283236+ra3umist@users.noreply.github.com>
2020-02-11 12:46:26 +0300

    Update LogInPage.java

--- src/main/java/habr/LogInPage.java
+++ src/main/java/habr/LogInPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 import java.util.concurrent.TimeUnit;
 
 public class LogInPage {
-    WebDriver driver;
+    private WebDriver driver;
 
     @FindBy(xpath = "//input[@id='email_field']")
     private WebElement emailField;
